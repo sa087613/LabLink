646774b68fb067fc103e9fe09b86e5f7d162b37e
Soph Aban <abanadorsophia@gmail.com>
2026-09-11 15:46:36 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "LabLink",
-  description: "LabLink",
+  description: "LabLink Website",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
